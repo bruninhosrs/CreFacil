df04e2870a63b41dbba0c180b3542195fef6c77d
bruninhosrs <brunorodrigues398@gmail.com>
2025-10-09 18:36:02 -0300

    Inicializando o JS

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const form = document.querySelector("#formCrefacil");
+const nomeInput = document.querySelector("#name");
+const cpfInput = document.querySelector("#cpf");
+const celularInput = document.querySelector("#celular");
+const celularRefInput = document.querySelector("#celularRef");
 
+const validarCampo = () => {
+  const nome = nomeInput.value;
+  const cpf = cpfInput.value;
+  const phone = celularInput.value;
+  const phoneRef = celularRefInput.value;
+  console.log(
+    `Nome: ${nome} CPF ${cpf} Celular ${phone} Celular Ref ${phoneRef}`
+  );
+};
+
+const enviarForm = (e) => {
+  e.preventDefault();
+
+  validarCampo();
+};
+
+form.addEventListener("submit", enviarForm);
